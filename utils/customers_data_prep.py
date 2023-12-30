a142e01a144cd75edd0008887ed77b1fec38f5a8
Gabriel Mota Bromonschenkel Lima <gabriel.mota.b.lima@gmail.com>
2023-12-30 09:34:25 -0300

    :tada: first analysis

---
+++ utils/customers_data_prep.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from dataclasses import dataclass
+
+@dataclass
+class CustomersDataPrep:
+
+    binary_features = [
+        "gender",
+        "Partner",
+        "Dependents",
+        "PhoneService",
+        "PaperlessBilling",
+        "Churn"
+    ]
+
+    conditional_features = [
+        "MultipleLines",    
+        "OnlineSecurity",
+        "OnlineBackup",
+        "DeviceProtection",
+        "TechSupport",
+        "StreamingTV",
+        "StreamingMovies"
+    ]
+
+    drop_columns = ["customerID"]
+
+    comma_string_numbers = ["MonthlyCharges", "TotalCharges"]
+
+    def __comma_to_number(
+            self,
+            series: pd.Series,
+            typing: type=float
+        ) -> pd.Series:
+        return series.fillna("0").str.replace(",", ".").astype(typing)
+
+    def __comma_numeric_string(
+            self,
+            df: pd.DataFrame
+        ) -> pd.DataFrame:
+        for feature in self.comma_string_numbers:
+            df[feature] =      self.__comma_to_number(df[feature]) \
+                               if   df[feature].dtype == "object" \
+                               else df[feature]
+        return df
+
+    def __binarize_features(
+            self,
+            df: pd.DataFrame,
+            conditionals:bool=False
+        ) -> pd.DataFrame:
+        if conditionals:
+            for feature in self.conditional_features:
+                df[feature] = df[feature].mask(df[feature] != "Yes", "No").map({"Yes": 1, "No": 0})
+
+        for feature in self.binary_features:
+            df[feature] =      df[feature].map({"Yes": 1, "No": 0}) \
+                          if   feature != "gender" \
+                          else df[feature].map({"Male": 1, "Female": 0})    
+        return df
+
+    def prepare_data(
+            self,
+            df: pd.DataFrame,
+            conditionals: bool = False
+        ) -> pd.DataFrame:
+        df = df.drop(columns=self.drop_columns, errors="ignore") \
+            .pipe(self.__comma_numeric_string) \
+            .pipe(self.__binarize_features, conditionals)
+        return df
---
+++ utils/utils.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+from sklearn.metrics import (
+    silhouette_score,
+    davies_bouldin_score,
+    calinski_harabasz_score,
+    adjusted_rand_score,
+    adjusted_mutual_info_score,
+    homogeneity_completeness_v_measure,
+    fowlkes_mallows_score
+)
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
+def df_columns(df: pd.DataFrame):
+    numerical_columns = ["tenure", "MonthlyCharges", "TotalCharges"]
+    categorical_columns = list(set(df.columns.tolist()) - set(numerical_columns) - set(["customerID"]))
+    return numerical_columns, categorical_columns
+
+
+def cluster_metrics(model, data, labels_true, labels_pred):
+    silhouette = silhouette_score(data, labels_pred)
+    davies_bouldin = davies_bouldin_score(data, labels_pred)
+    calinski_harabasz = calinski_harabasz_score(data, labels_pred)
+
+    print(f"Results for {model}:")
+    print(f"Silhouette Score: {silhouette}")
+    print(f"Davies-Bouldin Score: {davies_bouldin}")
+    print(f"Calinski-Harabasz Score: {calinski_harabasz}")
